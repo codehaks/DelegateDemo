762b288a637abde418251e7e40cde02b77e88ef8
Hakim Ghods <ghods.hakim@hotmail.com>
2024-01-21 16:26:32 +0330

    Add project files.

---
+++ DelegateDemo/Program.cs
@@ -0,0 +1,46 @@
+﻿
+var input = "This is my input text!";
+
+string select=Console.ReadLine();
+
+Func<string,string> method;
+
+switch (select)
+{
+    case "L":
+        method = ConvertToLower;
+        break;
+    case "U":
+        method = ConvertToUpper;
+        break;
+    case "N":
+        method = ConvertToNoSpaces;
+        break;
+    default:
+        method = ConvertToLower;
+        break;
+}
+
+var output= method(input);
+
+Console.WriteLine(output);
+
+
+string ConvertToLower(string input)
+{
+    var output=input.ToLower();
+    return output;
+}
+
+string ConvertToUpper(string input)
+{
+    var output = input.ToUpper();
+    return output;
+}
+
+string ConvertToNoSpaces(string input)
+{
+    return input.Replace(" ", "");
+}
+
+//delegate string ConvertMethod(string input);
